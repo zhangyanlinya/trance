0409885cea500c8afe23bd2fa11c5c342c3daa6d
zhangyanlinya <zhangyanlinya@126.com>
2016-11-10 11:08:02 +0800

    trance first

---
+++ android/src/com/trance/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.trance.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.trance.TranceGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new TranceGame(), config);
+	}
+}
---
+++ desktop/src/com/trance/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.trance.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.trance.TranceGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TranceGame(), config);
+	}
+}
