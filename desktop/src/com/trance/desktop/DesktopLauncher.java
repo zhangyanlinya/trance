9a05793e92744841e5a6e00dd0fb21db84bced84
zhangyanlinya <zhangyanlinya@126.com>
2016-11-11 20:17:31 +0800

    destop

--- desktop/src/com/trance/desktop/DesktopLauncher.java
+++ desktop/src/com/trance/desktop/DesktopLauncher.java
@@ -2,7 +2,7 @@ package com.trance.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-
+import com.trance.view.TranceGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
