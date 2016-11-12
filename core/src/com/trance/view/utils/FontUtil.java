f4ebb7b122efbeff49be3a9e6fde27b46a49a12a
zhangyanlinya <zhangyanlinya@126.com>
2016-11-12 19:41:33 +0800

    freefont

--- core/src/com/trance/view/utils/FontUtil.java
+++ core/src/com/trance/view/utils/FontUtil.java
@@ -6,7 +6,8 @@ import var3d.net.freefont.FreeFont;
 public class FontUtil {
 
 	private static FreeBitmapFont font;
-	public static final java.lang.String DEFAULT_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+	public static final java.lang.String DEFAULT_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890"+
+			"\"!`?'.,;:()[]{}<>|/@\\^$-%+=#_&~*\u007F\u0080\u0081\u0082\u0083\u0084\u0085\u0086\u0087\u0088\u0089\u008A\u008B\u008C\u008D\u008E\u008F\u0090\u0091\u0092\u0093\u0094\u0095\u0096\u0097\u0098\u0099\u009A\u009B\u009C\u009D\u009E\u009F ¡¢£¤¥¦§¨©ª«¬\u00AD®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿ";
 
 	public static FreeBitmapFont getFont(){
 		if(font == null){
