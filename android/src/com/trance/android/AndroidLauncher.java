b16c3e260c93de571b0424b5a08a6610f2c6ab1c
zhangyanlinya <zhangyanlinya@126.com>
2016-12-19 19:20:10 +0800

    box2d

--- android/src/com/trance/android/AndroidLauncher.java
+++ android/src/com/trance/android/AndroidLauncher.java
@@ -150,7 +150,7 @@ public class AndroidLauncher extends AndroidApplication {
 
 		GetDeviceId getDeviceId  = new GetDeviceId(this);
 		Player.userName = getDeviceId.getCombinedId();
-//		Player.userName ="and3";
+//		Player.userName ="gruby";
 
 		//
 
